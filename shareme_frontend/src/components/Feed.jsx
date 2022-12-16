1d74d665f90191bdc53a10509ffc8b30c156f7b7
Mike Green <greens1593@gmail.com>
2022-12-16 20:36:26 +0200

    Завершена разработка сайдбара. Начата разработка секции pins. Сделан навбар и спиннер

---
+++ shareme_frontend/src/components/Feed.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect }  from 'react'
+
+import { useParams } from 'react-router-dom'
+
+import client from '../client'
+import {searchQuery} from '../utils/data'
+
+import MasonryLayout from './MasonryLayout'
+import Spinner from './Spinner' 
+
+const Feed = () => {
+    const [loading, setLoading] = useState(false)
+    const { categoryId } = useParams();
+
+    useEffect(() => {
+        setLoading(true)
+
+        if (categoryId) {
+            const query = searchQuery(categoryId)  
+        } else {
+
+        }
+    }, [input])
+    
+
+    if (loading) return <Spinner message='We are adding new ideas to your feed'/>
+
+  return (
+    <div>Feed</div>
+  )
+}
+
+export default Feed
--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -6,8 +6,8 @@ import { IoIosArrowForward } from 'react-icons/io'
 
 import logo from '../assets/logo.png'
 
-const isNotActiveStyle = "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize"
-const isActiveStyle = "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize"
+const isNotActiveStyle = "flex items-center mb-3 px-5 gap-3 text-gray-500 hover:text-black transition-font duration-200 ease-in-out capitalize"
+const isActiveStyle = "flex items-center mb-3 px-5 gap-3 font-extrabold border-r-2 border-black transition-font duration-200 ease-in-out capitalize"
 
 const categories = [
   { name: 'Animals' },
@@ -43,7 +43,7 @@ const Sidebar = ({ user, closeToggle }) => {
             <RiHomeFill /> 
             Home
           </NavLink>
-          <h3 className='mt-2 px-5 text-base 2xl:text-xl'>Disconer categories</h3>
+          <h3 className='mt-2 mb-5 px-5 text-base 2xl:text-xl'>Disconer categories</h3>
         </div>
         {categories.slice(0, categories.length - 1).map(category => (
           <NavLink
@@ -60,6 +60,7 @@ const Sidebar = ({ user, closeToggle }) => {
         <Link
           to={`user-profile/${user._id}`}
           className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleCloseSidebar}
         >
           <img src={user.image} alt="user-profile" className='w-10 h-10 rounded-full' />
           <p>{user.userName}</p>
